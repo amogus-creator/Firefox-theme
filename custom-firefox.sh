1485c66e4fc1e12975ae9892d35c910d9ecfc2ac
amogus-creator <t84214583@gmail.com>
2024-10-27 16:30:43 +0300

    Update and rename auto.sh to custom-firefox.sh

---
+++ custom-firefox.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i '39s|^..||' /home/luvsxf/.mozilla/firefox/firefox-themes/userChrome.css
+
+sed -i '/padding-left:/s|3|1' /home/luvsxf/.mozilla/firefox/firefox-themes/WhiteSur/hide-window-buttons.css
+
+sed -i '8a padding-right: 1px; !important' /home/luvsxf/.mozilla/firefox/firefox-themes/WhiteSur/hide-window-buttons.css
