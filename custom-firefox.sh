9d3cf7a4c9f2e6e7f79cfa13163753e6197f755b
amogus-creator <t84214583@gmail.com>
2024-10-27 16:43:58 +0300

    Update custom-firefox.sh

--- custom-firefox.sh
+++ custom-firefox.sh
@@ -4,4 +4,4 @@ sed -i '39s|^..||' /home/luvsxf/.mozilla/firefox/firefox-themes/userChrome.css
 
 sed -i '/padding-left:/s|3|1' /home/luvsxf/.mozilla/firefox/firefox-themes/WhiteSur/hide-window-buttons.css
 
-sed -i '8a padding-right: 1px; !important' /home/luvsxf/.mozilla/firefox/firefox-themes/WhiteSur/hide-window-buttons.css
+sed -i '8a padding-right: 1px !important;' /home/luvsxf/.mozilla/firefox/firefox-themes/WhiteSur/hide-window-buttons.css
